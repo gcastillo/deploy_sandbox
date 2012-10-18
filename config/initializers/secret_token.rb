ad397d3058485a5d2e4dcba41a6bc8ab0f25c2d9
Gerardo Castillo <gerardo.castillo@avantica.net>
2012-10-18 13:28:04 -0700

    Initial app structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeploySandbox::Application.config.secret_token = '5d4a274f2491b46708b176fd86cca83a719e41d58c252209f00c9f9d9179b2a4017f40e9f209548d5ee2c829666c07d6396c6566601cacfb49a327dbf30a7dc9'
